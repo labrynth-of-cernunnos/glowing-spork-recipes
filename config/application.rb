f82500e847aac03945a1db6a474d13ef5a76fd6c
capricious-slingshot <ephemere@protonmail.com>
2020-08-29 18:58:30 -0700

    adding nav bar and cards to index page

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ module GlowingSporkRecipes
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    #adding asset pipeline image folder
+    config.assets.enabled = true
+    config.assets.paths << Rails.root.join("app", "assets", "images")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
