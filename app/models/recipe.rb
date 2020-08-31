ff5e5217caf5b38c6a30d9c65f2b1d7044814829
capricious-slingshot <ephemere@protonmail.com>
2020-08-30 18:31:25 -0700

    adding courses association to recipes

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,4 @@
 class Recipe < ApplicationRecord
   has_and_belongs_to_many :styles
+  belongs_to :course
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200831002120) do
+ActiveRecord::Schema.define(version: 20200831003843) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string "photo"
